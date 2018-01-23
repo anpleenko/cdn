76495ebd3cd46619ab878ad69fbb5b554d261acd
Alexander Anpleenko <vaeum@outlook.com>
2018-01-23 23:15:27 +0300

    Update run_commands_after_ubuntu_install.sh

--- server/Ubuntu/run_commands_after_ubuntu_install.sh
+++ server/Ubuntu/run_commands_after_ubuntu_install.sh
@@ -1,20 +1,6 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install mc zsh python3-pip ruby-full -y
-
-pip3 install --upgrade pip
-sudo pip3 install docker-compose
-
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get install nodejs -y
-
-# install global npm packages
-sudo npm i -g npm npm-check nodemon webpack babel-cli eslint gulp np
-
-echo "depth=0" >> ~/.npmrc
-echo "init.author.name=Alexander Anpleenko" >> ~/.npmrc
-echo "init.author.email=vaeum@outlook.com" >> ~/.npmrc
-echo "init.author.url=http://vaeum.com" >> ~/.npmrc
+sudo apt-get install mc zsh -y
 
 echo "alias ..='cd ..'" >> ~/.zshrc
 echo "alias ...='cd ../..'" >> ~/.zshrc
@@ -27,9 +13,13 @@ source ~/.zshrc
 # install docker
 wget -qO- https://get.docker.com/ | sh
 
-sudo apt-get clean
+# install docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # install vim config
 curl https://raw.githubusercontent.com/vaeum/cdn/master/server/vim/vimrc > ~/.vimrc
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+sudo apt-get clean
