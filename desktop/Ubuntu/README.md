cd1a80159cff0f71f638feb1599e7d29bde8b396
Alexander <vaeum@mail.ru>
2018-02-11 13:35:09 +0300

    add desctop ubuntu install

---
+++ desktop/Ubuntu/README.md
@@ -0,0 +1,7 @@
+### Install
+
+Copy-paste this code in terminal and run it
+
+```
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/vaeum/cdn/master/desktop/Ubuntu/install.sh)"
+```
---
+++ desktop/Ubuntu/install.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+cd ~
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install mc zsh vim -y
+
+echo "alias ..='cd ..'" >> ~/.zshrc
+echo "alias ...='cd ../..'" >> ~/.zshrc
+echo "alias ....='cd ../../..'" >> ~/.zshrc
+echo "alias c='clear'" >> ~/.zshrc
+echo "alias del='sudo rm -rf'" >> ~/.zshrc
+echo "alias sources='source ~/.zshrc'" >> ~/.zshrc
+source ~/.zshrc
+
+# install google chrome
+# http://help.ubuntu.ru/wiki/google_chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update
+sudo apt-get install google-chrome-stable -y
+
+# install nodejs
+# https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# install sublime-text
+# https://www.sublimetext.com/docs/3/linux_repositories.html
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text -y
+
+# install atom
+# http://www.webupd8.org/2014/06/atom-text-editor-available-for-linux.html
+sudo add-apt-repository ppa:webupd8team/atom -y
+sudo apt-get update
+sudo apt-get install atom -y
+
+# install telegram
+# https://launchpad.net/~atareao/+archive/ubuntu/telegram
+sudo add-apt-repository ppa:atareao/telegram -y
+sudo apt-get update
+sudo apt-get install telegram -y
+
+# install Rhythmbox
+# http://ubuntuhandbook.org/index.php/2017/10/rhythmbox-3-4-2-release-how-to-install-ppa/
+sudo add-apt-repository ppa:ubuntuhandbook1/apps -y
+sudo apt-get update
+sudo apt-get install rhythmbox -y
+
+# install gitkraken
+# https://gist.github.com/developer-prosenjit/93d534ce3dc8442be16a990abd678f10
+wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+sudo dpkg -i --force-depends gitkraken-amd64.deb
+sudo apt-get install -f
+rm -f gitkraken-amd64.deb
+
+# install docker
+wget -qO- https://get.docker.com/ | sh
+
+# install docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# install vim config
+curl https://raw.githubusercontent.com/vaeum/cdn/master/server/vim/vimrc > ~/.vimrc
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+sudo apt-get clean
