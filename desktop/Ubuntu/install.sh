c8b2782c1b98ed7b2166dc81fba37061863429d3
Alexander Anpleenko <vaeum@mail.ru>
2019-04-23 19:21:58 +0300

    fix install docker-compose

--- desktop/Ubuntu/install.sh
+++ desktop/Ubuntu/install.sh
@@ -101,7 +101,7 @@ sudo apt-get update && sudo apt-get install yarn -y
 wget -qO- https://get.docker.com/ | sh
 
 # install docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # install vim config
